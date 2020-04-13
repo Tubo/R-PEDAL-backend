128d4b9f4e003c830248a26c653201824bd6c614
Tubo Shi <shitubo@gmail.com>
2020-04-13 20:53:25 +1200

    add admin interface

--- api/admin.py
+++ api/admin.py
@@ -3,14 +3,21 @@ from django.contrib import admin
 from .models import *
 
 
+class LesionAdmin(admin.TabularInline):
+    model = Lesion
+    min_num = 0
+    max_num = 2
+
+
 @admin.register(Entry)
 class EntryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("timestamp", "patient_id", "mri_date", "lesion_num", "psa_level", "ece", "svi")
 
+    inlines = [LesionAdmin]
 
-@admin.register(Lesion)
-class LesionAdmin(admin.ModelAdmin):
-    pass
+    def lesion_num(self, e):
+        return e.lesions.count()
+    lesion_num.short_description = "Number of lesions"
 
 
 @admin.register(Location)
