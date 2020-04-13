aaed6d5d0ae7c667d758effd092522e1df3a5c52
Tubo Shi <shitubo@gmail.com>
2020-04-13 20:59:00 +1200

    change admin title

--- r_pedal_backend/urls.py
+++ r_pedal_backend/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from api.views import EntryListCreateAPIView
 
+admin.site.site_header = "R-PEDAL Project"
+admin.site.site_title = "R-PEDAL Project"
+admin.site.index_title = "Welcome to the R-PEDAL Project"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", EntryListCreateAPIView.as_view(), name="entry-view")
