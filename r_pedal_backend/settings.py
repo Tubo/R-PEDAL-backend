b51913e25474ecd60705381260e800195ae04d76
Tubo Shi <shitubo@gmail.com>
2020-04-21 00:57:53 +1200

    install django-import-export

--- r_pedal_backend/settings.py
+++ r_pedal_backend/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.admin",
     "rest_framework",
     "corsheaders",
+    'import_export',
     "api.apps.ApiConfig",
 ]
 
@@ -140,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # CORS headers
 CORS_ORIGIN_WHITELIST = [
